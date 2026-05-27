dbd161982d8e8bcaf4382ff0ccccac8a6fc58aed
PrudkiyD <dmitro.prudkiy@gmail.com>
2026-05-27 10:58:10 +0300

    test ci/cd

--- laravel/app/Filament/Widgets/TopCustomersTable.php
+++ laravel/app/Filament/Widgets/TopCustomersTable.php
@@ -30,7 +30,7 @@ class TopCustomersTable extends BaseWidget
                     )
                     ->groupBy('user_customer.id', 'user_customer.name', 'user_customer.phone')
                     ->orderByDesc('total_spent')
-                    ->limit(5)
+                    ->limit(6)
             )
             ->columns([
                 Tables\Columns\TextColumn::make('name')
