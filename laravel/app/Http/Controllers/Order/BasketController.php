71f5d01610908c807f94ae39b567a1f1eea6724b
PrudkiyD <dmitro.prudkiy@gmail.com>
2026-05-28 19:57:27 +0300

    product

--- laravel/app/Http/Controllers/Order/BasketController.php
+++ laravel/app/Http/Controllers/Order/BasketController.php
@@ -130,9 +130,30 @@ class BasketController extends Controller
         $item->price = $price;
         $item->save();
 
+        $product = $item;
+
         // Оновлюємо total кошика
         $this->updateBasketTotal($basket);
 
+        // Створюємо унікальний ключ для сесії, наприклад: 'viewed_products.5'
+        $sessionKey = 'viewed_products.' . $product->id;
+
+        // Перевіряємо, чи немає цього ключа в сесії
+        if (!$request->session()->has($sessionKey)) {
+            
+            
+            // Записуємо перегляд у базу даних
+            View::create([
+                'product_id' => $product->id,
+                'user_id'    => auth()->check() ? auth()->id() : null, // ID юзера, якщо залогінений
+                'ip_address' => $request->ip(),                        // IP-адреса відвідувача
+            ]);
+
+            // Позначаємо в сесії, що цей товар у поточній сесії вже "подівилися"
+            $request->session()->put($sessionKey, true);
+        }
+
         return response()->json(['price' => $price, 'prices' => $prices]);
     }
+
 }
