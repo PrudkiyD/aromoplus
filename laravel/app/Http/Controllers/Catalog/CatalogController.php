63260e70655ae1991a58f614c57f2b225829b052
PrudkiyD <dmitro.prudkiy@gmail.com>
2026-05-28 18:59:49 +0300

    view log

--- laravel/app/Http/Controllers/Catalog/CatalogController.php
+++ laravel/app/Http/Controllers/Catalog/CatalogController.php
@@ -9,7 +9,6 @@ use App\Models\Page\Element;
 use App\Models\Catalog\Product;
 use App\Models\Catalog\Price;
 use Illuminate\Http\Request;
-use App\Models\Catalog\View;
 
 class CatalogController extends Controller
 {
@@ -74,26 +73,6 @@ class CatalogController extends Controller
 
         // Отримуємо ID прайс-листа
         $price_list_id = $request->cookie('user_price', 1);
-
-        // Створюємо унікальний ключ для сесії, наприклад: 'viewed_products.5'
-        $sessionKey = 'viewed_products.' . $product->id;
-        dd($request->ip());
-
-        // Перевіряємо, чи немає цього ключа в сесії
-        if (!$request->session()->has($sessionKey)) {
-            
-            
-            // Записуємо перегляд у базу даних
-            View::create([
-                'product_id' => $product->id,
-                'user_id'    => auth()->check() ? auth()->id() : null, // ID юзера, якщо залогінений
-                'ip_address' => $request->ip(),                        // IP-адреса відвідувача
-            ]);
-
-            // Позначаємо в сесії, що цей товар у поточній сесії вже "подівилися"
-            $request->session()->put($sessionKey, true);
-        }
-        
 
         return view('catalog.product', compact(
             'title',
