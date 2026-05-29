44bfde04bc8c9f0c871cca43df619531eeba8c43
PrudkiyD <dmitro.prudkiy@gmail.com>
2026-05-29 14:55:10 +0300

    product tabel fil

--- laravel/app/Filament/Resources/Catalog/ProductResource.php
+++ laravel/app/Filament/Resources/Catalog/ProductResource.php
@@ -291,9 +291,6 @@ class ProductResource extends Resource
                     ->multiple()
                     ->searchable(),
             ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
             ]);
