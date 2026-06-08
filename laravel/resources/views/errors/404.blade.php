994eda537f08938a249f0239f828552cf1c44bcf
PrudkiyD <dmitro.prudkiy@gmail.com>
2026-06-08 10:25:16 +0300

    404 page

--- laravel/resources/views/errors/404.blade.php
+++ laravel/resources/views/errors/404.blade.php
@@ -1,12 +1,9 @@
-{{-- resources/views/errors/404.blade.php --}}
-@extends('layouts.app')
 
-@section('content')
-    <div class="flex flex-col items-center justify-center min-h-screen">
-        <h1 class="text-6xl font-bold">404</h1>
-        <p class="text-xl mt-4">Сторінку не знайдено</p>
-        <a href="{{ url('/') }}" class="mt-6 btn btn-primary">
-            На головну
-        </a>
-    </div>
-@endsection
+
+<div class="flex flex-col items-center justify-center min-h-screen">
+<h1 class="text-6xl font-bold">404</h1>
+<p class="text-xl mt-4">Сторінку не знайдено</p>
+<a href="{{ url('/') }}" class="mt-6 btn btn-primary">
+    На головну
+</a>
+</div>
