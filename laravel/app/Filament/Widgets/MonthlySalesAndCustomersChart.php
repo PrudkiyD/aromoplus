5c62d35f67b8b848324a5f3a70739f1d64319138
PrudkiyD <dmitro.prudkiy@gmail.com>
2026-06-08 11:54:28 +0300

    404 pag

--- laravel/app/Filament/Widgets/MonthlySalesAndCustomersChart.php
+++ laravel/app/Filament/Widgets/MonthlySalesAndCustomersChart.php
@@ -64,7 +64,7 @@ class MonthlySalesAndCustomersChart extends ChartWidget
                 [
                     'label' => 'Продажі (₴)',
                     'data' => $salesData,
-                    'borderColor' => '#f2b423',
+                    'borderColor' => '#ffd676',
                     'backgroundColor' => 'rgba(185, 131, 16, 0.07)',
                     'fill' => true,
                     'tension' => 0.4,
